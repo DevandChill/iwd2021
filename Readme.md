4a23497b524bf1cde16eaf528932dde173cf2e4e
Hope Atina <hopeatina@users.noreply.github.com>
2021-03-08 23:38:48 -0600

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+Happy International Womens Day!!
+[https://codepen.io/lbebber/pen/xrwja](https://codepen.io/lbebber/pen/xrwja)
+
+Grab a div, animate it :)
+
+Sponsored by Red Lobster
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import "./hope.css";
+import "./teia.css";
+import "./eric.css";
+import "./codi.css";
+import "./christina.css";
+import "./andre.css";
+import "./Lennox.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Happy International Women's Day!!!</h1>
+<h1 class="codis-class">Happy International Women's Day!!!</h1>
+`;
